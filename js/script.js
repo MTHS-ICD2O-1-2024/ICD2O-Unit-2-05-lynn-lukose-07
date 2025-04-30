89e88cb9ec48c1421a3681b83c461fbb708435e4
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-30 14:37:09 +0000

    edited js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lynn Lukose
+// Created on: March 2025
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -9,18 +9,20 @@
 /**
  * This function calculates the take home pay from a job.
  */
-function calculateTakeHomePay () {
-    const TAX_RATE = 0.18
+function calculateTakeHomePay() {
+  const TAX_RATE = 0.18
 
   // input
-  const hoursWorked = parseFloat(document.getElementById('hours-worked').value)
-  const hourlyWage = parseFloat(document.getElementById('hourly-wage').value)
+  const hoursWorked = parseFloat(document.getElementById("hours-worked").value)
+  const hourlyWage = parseFloat(document.getElementById("hourly-wage").value)
 
   // process
-  const takeHomeSalary = (hourlyWage * hoursWorked) * (1.00 - TAX_RATE)
-  const taxes = (hourlyWage * hoursWorked) * TAX_RATE
+  const takeHomeSalary = hourlyWage * hoursWorked * (1.0 - TAX_RATE)
+  const taxes = hourlyWage * hoursWorked * TAX_RATE
 
   // output
-  document.getElementById('pay').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
-  document.getElementById('taxes').innerHTML = 'The government will take: $' + taxes.toFixed(2)
+  document.getElementById("pay").innerHTML =
+    "Your pay will be: $" + takeHomeSalary.toFixed(2)
+  document.getElementById("taxes").innerHTML =
+    "The government will take: $" + taxes.toFixed(2)
 }
